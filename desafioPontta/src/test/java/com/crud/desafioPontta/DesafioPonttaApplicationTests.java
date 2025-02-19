8106b4218b350dfeb1b46ad87efd85929bb6fc70
gabrielneeeves <gabrielnevesferreira08@gmail.com>
2025-02-19 14:04:07 -0300

    first commit

---
+++ desafioPontta/src/test/java/com/crud/desafioPontta/DesafioPonttaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.desafioPontta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPonttaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
